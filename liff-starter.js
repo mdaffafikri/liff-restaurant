d2808de7025de852e0968ed13a5328c39037925d
daps <daffafikri@ymail.com>
2020-12-07 13:22:57 +0700

    idk

--- liff-starter.js
+++ liff-starter.js
@@ -97,12 +97,13 @@ function displayLiffData() {
 */
 function displayIsInClientInfo() {
     if (liff.isInClient()) {
-        document.getElementById('liffLoginButton').classList.toggle('hidden');
-        document.getElementById('liffLogoutButton').classList.toggle('hidden');
-        document.getElementById('isInClientMessage').textContent = 'You are opening the app in the in-app browser of LINE.';
-    } else {
-        document.getElementById('isInClientMessage').textContent = 'You are opening the app in an external browser.';
-    }
+        document.getElementById('login').classList.toggle('hidden');
+        document.getElementById('logout').classList.toggle('hidden');
+        // document.getElementById('isInClientMessage').textContent = 'You are opening the app in the in-app browser of LINE.';
+    } 
+    // else {
+    //     document.getElementById('isInClientMessage').textContent = 'You are opening the app in an external browser.';
+    // }
 }
 
 function registerButtonHandlers() {
