c3efbe5f330a9f7f1db7763190251af938e1d9d3
daps <daffafikri@ymail.com>
2020-12-09 11:15:18 +0700

    Random commit

--- liff-starter.js
+++ liff-starter.js
@@ -99,12 +99,12 @@ function displayLiffData() {
 /**
 * Toggle the login/logout buttons based on the isInClient status, and display a message accordingly
 */
-function displayIsInClientInfo() {
-    if (liff.isInClient()) {
-        $("#login").addClass('hidden'); 
-        $("#logout").addClass('hidden');   
-    } 
-}
+// function displayIsInClientInfo() {
+//     if (liff.isInClient()) {
+//         $("#login").addClass('hidden'); 
+//         $("#logout").addClass('hidden');   
+//     } 
+// }
 
 function registerButtonHandlers() {
 
@@ -151,19 +151,113 @@ function registerButtonHandlers() {
     });
 }
 
+
+
 // function sendAlertIfNotInClient() {
 //     alert('Tidak bisa memesan melalui external browser!');
 // }
  
-/**
-* Toggle specified element
-* @param {string} elementId The ID of the selected element
-*/
-function toggleElement(elementId) {
-    const elem = document.getElementById(elementId);
-    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-        elem.style.display = 'none';
-    } else {
-        elem.style.display = 'block';
-    }
+// function toggleElement(elementId) {
+//     const elem = document.getElementById(elementId);
+//     if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+//         elem.style.display = 'none';
+//     } else {
+//         elem.style.display = 'block';
+//     }
+// }
+
+
+// batas kode liff
+
+var pesanan = {
+    burger: 0,
+    frenchFries: 0,
+    icedTea: 0,
+    hotCoffee: 0
 }
+var totalHarga = 0;
+
+$(document).on('click', '.tambah', function (event) {
+    if ($(this).data("pilihan") === 'burger') {
+        pesanan.burger++
+        totalHarga += 10000
+        $("#jumlahBurger").html(pesanan.burger)
+    }
+
+    if ($(this).data("pilihan") === 'frenchFries') {
+        pesanan.frenchFries++
+        totalHarga += 7000
+        $("#jumlahFrenchFries").html(pesanan.frenchFries)
+    }
+
+    if ($(this).data("pilihan") === 'icedTea') {
+        pesanan.icedTea++
+        totalHarga += 3000
+        $("#jumlahIcedTea").html(pesanan.icedTea)
+    }
+
+    if ($(this).data("pilihan") === 'hotCoffee') {
+        pesanan.hotCoffee++
+        totalHarga += 5000
+        $("#jumlahHotCoffee").html(pesanan.hotCoffee)
+    }
+    $("#total").html(`Burger ${pesanan.burger}, French Fries ${pesanan.frenchFries}, Iced Tea ${pesanan.icedTea}, Hot Coffee ${pesanan.hotCoffee}`);
+    $("#totalHarga").html(totalHarga);
+});
+
+$(document).on("click", '.kurang', function (event) {
+    if ($(this).data("pilihan") === 'burger') {
+        if (pesanan.burger < 1) {
+            pesanan.burger = 0
+        }
+        else {
+            totalHarga -= 10000
+            pesanan.burger--
+            $("#jumlahBurger").html(pesanan.burger)
+        }
+    }
+
+    if ($(this).data("pilihan") === 'frenchFries') {
+        if (pesanan.frenchFries < 1) {
+            pesanan.frenchFries = 0
+        }
+        else {
+            totalHarga -= 7000
+            pesanan.frenchFries--
+            $("#jumlahFrenchFries").html(pesanan.frenchFries)
+        }
+    }
+
+    if ($(this).data("pilihan") === 'icedTea') {
+        if (pesanan.icedTea < 1) {
+            pesanan.icedTea = 0
+        }
+        else {
+            totalHarga -= 3000
+            pesanan.icedTea--
+            $("#jumlahIcedTea").html(pesanan.icedTea)
+        }
+    }
+
+    if ($(this).data("pilihan") === 'hotCoffee') {
+        if (pesanan.hotCoffee < 1) {
+            pesanan.hotCoffee = 0
+        }
+        else {
+            totalHarga -= 5000
+            pesanan.hotCoffee--
+            $("#jumlahHotCoffee").html(pesanan.hotCoffee)
+        }
+    }
+    $("#total").html(`Burger ${pesanan.burger}, French Fries ${pesanan.frenchFries}, Iced Tea ${pesanan.icedTea}, Hot Coffee ${pesanan.hotCoffee}`);
+    $("#totalHarga").html(totalHarga);   
+});
+
+$("#pesan").click(function(){
+    if(!totalHarga){
+        $("#modalFail").modal();
+    }
+    else{
+        $("#modalSuccess").modal();
+    }
+})
